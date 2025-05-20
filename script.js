4db7fab13ab89ce22394b061803ecbbbd8801cfc
bharathboienipalli <bharathboienipalli@gmail.com>
2025-05-20 08:23:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(arr) {
+  if (arr.length <= 1) return 0;
+
+  const heap = [...arr];
+  heap.sort((a, b) => a - b); 
+
+  let totalCost = 0;
+
+  while (heap.length > 1) {
+    const first = heap.shift();
+    const second = heap.shift();
+
+    const cost = first + second;
+    totalCost += cost;
+
+    heap.push(cost);
+    heap.sort((a, b) => a - b);
+  }
+
+  return totalCost;
 }
 
-module.exports=mincost;
+module.exports = mincost;
